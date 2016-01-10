f97d8dfbb7cb74fc72c2b9a9489449aac960a2d5
Emile Nijssen <emilenijssen@gmail.com>
2016-01-10 16:13:11 +0100

    fixes

--- examples/remote.js
+++ examples/remote.js
@@ -7,8 +7,19 @@ remote.connect({
 	address	: process.argv[2] || '192.168.0.109',
 	key		: process.argv[3] || '4d5b7754bb3cc6475ffb3ca09a14dee9'
 }, function( err, key ){
+	if( err ) return console.error('Error', err);
+	
+	remote.getInputs(console.log);
+	
+	remote.setInput("HDMI_1", console.log)
+
+//	remote.setChannel('3_13_1_925_2098_19401_1536', console.log)
+	return;
+	
+
+	
 	console.log('------- showing float --------');
-	remote.show_float( "Test", function( err, result ){
+	remote.showFloat( "Test", function( err, result ){
 		if( err ) return console.error('Error', err);
 		
 		console.log("You should've seen 'Test'")	
