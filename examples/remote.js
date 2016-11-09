ab3308db2f79c09b38a497a54a8761513ff9bb80
Emile Nijssen <emilenijssen@gmail.com>
2016-11-09 22:59:14 +0100

    add reconnect functionality

--- examples/remote.js
+++ examples/remote.js
@@ -1,37 +1,43 @@
 "use strict";
 
 var Remote = require('..').Remote;
-var remote = new Remote();
+var remote = new Remote({
+	debug	: false
+});
+
+remote.on('error', console.error)
 
 remote.connect({
-	address	: process.argv[2] || '192.168.0.109',
+	address	: process.argv[2] || '192.168.0.7',
 	key		: process.argv[3] || '4d5b7754bb3cc6475ffb3ca09a14dee9'
 }, function( err, key ){
 	if( err ) return console.error('Error', err);
-	
-	
+
+	var i = 0;
+
 	setInterval(function(){
-	console.log('------- showing float --------');
-	remote.showFloat( "Test", function( err, result ){
-		if( err ) return console.error('Error', err);
-		
-		console.log("You should've seen 'Test'")	
-		
-		console.log('------- getting channels --------');
-		remote.getChannels(function( err, channels ){
-			if( err ) return console.error('Error', err);	
-		
-			console.log('got %s channels', channels.length)
-			console.log('channel #1:', channels[0]);
-			
+		i++;
+		console.log('------- showing float --------');
+		remote.showFloat( "Test (" + i + ")", function( err, result ){
+			if( err ) return console.error('Error', err);
+
+			console.log("You should've seen 'Test (" + i + ")")
+
 			/*
-			console.log('------- disconnecting --------');
-			remote.disconnect(function( err, disconnected ){
+			console.log('------- getting channels --------');
+			remote.getChannels(function( err, channels ){
 				if( err ) return console.error('Error', err);
-				console.log('Disconnected');			
-			});
+
+				console.log('got %s channels', channels.length)
+				console.log('channel #1:', channels[0]);
+
+				console.log('------- disconnecting --------');
+				remote.disconnect(function( err, disconnected ){
+					if( err ) return console.error('Error', err);
+					console.log('Disconnected');
+				});
+			})
 			*/
 		})
-	})
 	}, 5000);
 })
