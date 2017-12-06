b651f300cc8ff0faf2a6fd3da20f265606648840
Emile Nijssen <emile@athom.nl>
2017-12-06 16:33:03 +0100

    remove consistent connection (buggy), open and close connection for every request

--- examples/remote.js
+++ examples/remote.js
@@ -3,38 +3,42 @@
 const WebOSDevice = require('..').WebOSDevice;
 
 let remote = new WebOSDevice({
-	address	: process.argv[2] || '192.168.1.61',
+	address	: process.argv[2] || '192.168.1.56',
 	key		: process.argv[3] || '78ca390e682a061e34fd6d5b890c7999',
-	debug	: true
+	debug	: false
 });
 
 remote.on('key', key => {
 	console.log('Key:', key);
 })
 
-//setInterval(sendToast, 1000);
 sendToast();
+getInputs();
 
-//var i = 0;
+function getInputs() {
+	console.log('getInputs()');
+	
+	remote.getInputs()
+		.then( console.log.bind( null, '[log]' ) )
+		.catch( console.error.bind( null, '[err]' ))
+}
 
 function sendToast() {
 	console.log('sendToast()');
 	
-	//if( ++i > 3 ) return;
-	
 	setTimeout(() => {
 	
-	let msg = new Date();
-	
-	remote.createToast(`Test float: ${msg}!`)
-		.then( result => {
-			console.log(`Result:`, result);
-			sendToast();
-		})
-		.catch( err => {
-			console.error(`Error:`, err);
-			sendToast();
-		});
+		let msg = new Date();
+		
+		remote.createToast(`Test float: ${msg}!`)
+			.then( result => {
+				console.log(`Result:`, result);
+				sendToast();
+			})
+			.catch( err => {
+				console.error(`Error:`, err);
+				sendToast();
+			});
 		
 	}, 1000);
 	
